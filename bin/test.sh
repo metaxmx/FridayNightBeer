2c2f861f3a446c84946008189542c95716d13e39
Christian Simon <simon@illucit.com>
2017-04-30 18:35:58 +0200

    Travis integration

--- bin/test.sh
+++ bin/test.sh
@@ -6,13 +6,13 @@ source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/build.sh"
 echo "[info]"
 echo "[info] ---- BUILDING FNB"
 echo "[info]"
-build "$@" "coverage compile"
+build "$@" coverage compile
 
 echo "[info]"
 echo "[info] ---- RUNNING TESTS"
 echo "[info]"
 
-build "$@" "test coverageReport"
+build "$@" test coverageReport
 
 echo "[info]"
 echo "[info] ALL TESTS PASSED"
